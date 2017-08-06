38bbe76fc63f9ec4a0ee2e2b3620f62e45aa7f10
s-kobets <s-kobets617@yandex.ru>
2017-08-07 00:03:39 +0300

    add App and create component input with button

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img class="logo" src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <CityInput />
+  </div>
+</template>
+
+<script>
+import CityInput from './components/search-input.vue';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js Ap +++p'
+    }
+  },
+  components: {
+    CityInput
+  }
+}
+</script>
+
+<style>
+@import 'normalize.css'
+
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+
+h1, h2
+  font-weight: normal
+
+ul
+  list-style-type: none
+  padding: 0
+
+.logo
+  width: 60px
+  height: 60px
+
+</style>
---
+++ src/components/search-input.vue
@@ -0,0 +1,24 @@
+<template>
+  <form>
+
+    <input type='text'  v-model='cityInput'>
+    <button type='submit' v-on:click='handleFormSubmit'>Add City</button>
+  </form>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    cityInput: '',
+    msg: 'Welcome to Your Vue.js Ap +++p'
+  }),
+
+  methods: {
+      handleFormSubmit: function(e) {
+        e.preventDefault();
+        console.log(this.cityInput)
+      }
+  }
+}
+</script>
