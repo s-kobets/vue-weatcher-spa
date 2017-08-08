a8d90b6f7e7cac72279a93408e472fa249630962
s-kobets <s-kobets617@yandex.ru>
2017-08-08 09:52:14 +0300

    template list citys

---
+++ src/components/city-list.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class='column'>
+    <li v-for='city in citys'>
+        <div className='city'>
+            <div className='city-block'>
+                <img :src=`http://openweathermap.org/img/w/${city.weather[0].icon}.png` alt='city.weather[0].description' />
+                <div className='city-block__description'>
+                    <strong> {{city.name}} </strong>
+                    <ul>
+                        <li>{{city.weather[0].description}}</li>
+                    </ul>
+                </div>
+            </div>
+            <a href='#' class='city-block__delete' title='delete'>&#215;</a>
+        </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    computed: mapState({
+        citys: state => state.citys
+    })
+}    
+</script>
